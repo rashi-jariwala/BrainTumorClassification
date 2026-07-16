0ffcd03288403892d93c7fb35890fd4d1682e4d5
RashiJariwala <rm.pharma1120@gmail.com>
2026-07-16 16:52:57 +0530

    port change

--- app.py
+++ app.py
@@ -20,4 +20,4 @@ app.register_blueprint(auth_bp)
 app.register_blueprint(dashboard_bp)
 app.register_blueprint(prediction_bp)
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=False)
